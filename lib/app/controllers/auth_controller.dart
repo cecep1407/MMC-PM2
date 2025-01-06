c0b06c7362dde5cd88d96f448b866939bc618812
heruarbianto <heru_arbianto@teknokrat.ac.id>
2025-01-06 13:39:07 +0700

    HERU: memperbaiki validasi login

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -18,17 +18,25 @@ class AuthController extends GetxController {
       );
       Get.offAllNamed(Routes.HOME);
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
+      // // Cetak nilai e.code ke konsol (untuk Debugging)
+      // print('Error Code: ${e.code}');
+      // print('Error Message: ${e.message}');
+      // // Tampilkan error melalui dialog(untuk debugging)
+      // Get.defaultDialog(
+      //   title: "Proses Gagal",
+      //   middleText: "Error Code: ${e.code}\nMessage: ${e.message}",
+      // );
+      if (e.code == 'invalid-email') {
         print('No user found for that email.');
         Get.defaultDialog(
-          title: "Proses Gagal",
-          middleText: "No user found for that email.",
+          title: "Login Gagal",
+          middleText: "User Tidak Ditemukan",
         );
-      } else if (e.code == 'wrong-password') {
+      } else if (e.code == 'invalid-credential') {
         print('Wrong password provided for that user.');
         Get.defaultDialog(
-          title: "Proses Gagal",
-          middleText: "Wrong password provided for that user.",
+          title: "Login Gagal",
+          middleText: "Password/Email salah",
         );
       }
     }
@@ -40,23 +48,23 @@ class AuthController extends GetxController {
   }
 
   void resetPassword(String email) async {
-  try {
-    await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-    Get.defaultDialog(
-      title: "Reset Password",
-      middleText: "Cek email Anda untuk instruksi reset password.",
-    );
-  } on FirebaseAuthException catch (e) {
-    String message = 'Terjadi kesalahan, coba lagi.';
-    if (e.code == 'user-not-found') {
-      message = 'Pengguna dengan email tersebut tidak ditemukan.';
-    } else if (e.code == 'invalid-email') {
-      message = 'Email yang Anda masukkan tidak valid.';
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      Get.defaultDialog(
+        title: "Reset Password",
+        middleText: "Cek email Anda untuk instruksi reset password.",
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = 'Terjadi kesalahan, coba lagi.';
+      if (e.code == 'user-not-found') {
+        message = 'Pengguna dengan email tersebut tidak ditemukan.';
+      } else if (e.code == 'invalid-email') {
+        message = 'Email yang Anda masukkan tidak valid.';
+      }
+      Get.defaultDialog(
+        title: "Proses Gagal",
+        middleText: message,
+      );
     }
-    Get.defaultDialog(
-      title: "Proses Gagal",
-      middleText: message,
-    );
   }
-}
 }
